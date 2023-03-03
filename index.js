030a6257810354305c9dec0b6a941caf431bb9a3
Ali-Assaad <aliassaadtr@gmail.com>
2023-03-03 15:05:33 +0200

    Clean code: selecting DOM elements

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const btnEl = document.getElementById("btn");
-const jokeEl = document.getElementById("joke");
+// Select DOM elements
+const button = document.getElementById("btn");
+const jokeElement = document.getElementById("joke");
 
 const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
 
