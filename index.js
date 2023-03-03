01017373de6472138d298be65e5d513784c6db8a
Ali-Assaad <aliassaadtr@gmail.com>
2023-03-03 14:36:34 +0200

    Add API necessary variables

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const btnEl = document.getElementById("btn");
 const jokeEl = document.getElementById("joke");
 
+const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
