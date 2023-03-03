8b18ea2160903693897b2d9f3f18e15998a98e9f
Ali-Assaad <aliassaadtr@gmail.com>
2023-03-03 14:38:47 +0200

    Add getJoke functionality

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ const options = {
 };
 
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
+    jokeEl.innerText = data[0].joke;
+}
+
+btnEl.addEventListener("click", getJoke);
