a8e9da1a120c536cdf75a632e0b600c975cc1090
Ali-Assaad <aliassaadtr@gmail.com>
2023-03-03 14:39:48 +0200

    Add a try catch block to the getJoke function

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const options = {
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 
 async function getJoke() {
-
+  try {
     jokeEl.innerText = "Updating...";
     btnEl.disabled = true;
     btnEl.innerText = "Loading...";
@@ -24,6 +24,12 @@ async function getJoke() {
     btnEl.innerText = "Tell me a joke";
 
     jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText = "An error happened, try again later";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+  }
 }
 
 btnEl.addEventListener("click", getJoke);
