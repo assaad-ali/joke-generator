18eac06b1b8efac6bfe984c6c6890323a6677ed6
Ali-Assaad <aliassaadtr@gmail.com>
2023-03-04 23:48:48 +0200

    object-oriented programming approach: Add constructor function

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-// Select DOM elements
-const button = document.getElementById("btn");
-const jokeElement = document.getElementById("joke");
+// Define the JokeGenerator class
+class JokeGenerator {
 
-// Define API endpoint and key
-const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
-const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
+  constructor(apiKey, apiURL) {
 
-// Define options for API request
-const options = {
-  method: "GET",
-  headers: {
-    "X-Api-Key": apiKey,
-  },
-};
+    // Define the API endpoint URL and query parameters as instance variables
+    this.apiKey = apiKey;
+    this.apiURL = apiURL;
 
-// Function to fetch and display joke
-async function getJoke() {
-  try {
-    // Show loading message and disable button
-    jokeElement.innerText = "Updating...";
-    button.disabled = true;
-    button.innerText = "Loading...";
+    this.options = {
+      method: "GET",
+      headers: {
+        "X-Api-Key": apiKey,
+      },
+    };
 
-    // Fetch joke from API
-    const response = await fetch(apiUrl, options);
-    const data = await response.json();
-
-    // Update button and display joke
-    button.disabled = false;
-    button.innerText = "Tell me a joke";
-    jokeElement.innerText = data[0].joke;
-  } catch (error) {
-    // Handle errors
-    jokeElement.innerText = "An error happened, try again later";
-    button.disabled = false;
-    button.innerText = "Tell me a joke";
-    console.log(error);
+    // Get references to the DOM elements for the button and joke text
+    this.button = document.getElementById("btn");
+    this.jokeElement = document.getElementById("joke");
   }
+
 }
 
-// Attach click event listener to button
-button.addEventListener("click", getJoke);
